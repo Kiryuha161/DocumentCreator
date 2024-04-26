139b9a568d2d3b9ac761f9bc08cd7177c90f00f9
Kiryuha161 <108215760+Kiryuha161@users.noreply.github.com>
2024-04-26 12:23:38 +0300

    Добавил CORS

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, pragma, cache-control'); 
+    next();
+  });
+
+
 app.post('/create-doc', async (req, res) => {
     try {
         const documentContent = fs.readFileSync(path.resolve(__dirname, "./Document.docx"), "binary");
